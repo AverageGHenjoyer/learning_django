ecec74a5596ad34d1de01ba772cc0c1fe52a621c
Maciej Kowalski <maciej.ko1444@gmail.com>
2024-01-02 13:32:20 +0100

    views and urls module completed

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:month>/", views.monthly_challenge_by_number),
-    path("<str:month>/", views.monthly_challenge, name="month-challenge")
+    path("", views.index), # /challenges/
+    path("<int:month>/", views.monthly_challenge_by_number), # /challenges/number
+    path("<str:month>/", views.monthly_challenge, name="month-challenge") # /challenges/month
 ]
